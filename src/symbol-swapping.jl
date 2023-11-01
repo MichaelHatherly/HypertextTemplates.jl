101b414696dd8c3f28ce52909b3ebcfcd1b0f3b3
Michael Hatherly <michaelhatherly@gmail.com>
2023-11-01 21:16:44 +0000

    Compat for Julia 1.6 `replace` missing method

--- src/symbol-swapping.jl
+++ src/symbol-swapping.jl
@@ -2,7 +2,7 @@ const __SPECIAL_AT_SYMBOL__ = "__special_at_symbol__"
 const __SPECIAL_DOLLAR_SYMBOL__ = "__special_dollar_symbol__"
 
 function _swap_special_symbols(s::String)::String
-    return replace(s, "@" => __SPECIAL_AT_SYMBOL__, "\$" => __SPECIAL_DOLLAR_SYMBOL__)
+    return _replace(s, "@" => __SPECIAL_AT_SYMBOL__, "\$" => __SPECIAL_DOLLAR_SYMBOL__)
 end
 
 function _restore_special_symbols(other)
@@ -10,5 +10,16 @@ function _restore_special_symbols(other)
 end
 
 function _restore_special_symbols(s::AbstractString)
-    return replace(s, __SPECIAL_AT_SYMBOL__ => "@", __SPECIAL_DOLLAR_SYMBOL__ => "\$")
+    return _replace(s, __SPECIAL_AT_SYMBOL__ => "@", __SPECIAL_DOLLAR_SYMBOL__ => "\$")
+end
+
+@static if VERSION >= v"1.7"
+    _replace(s, args...) = Base.replace(s, args...)
+else
+    function _replace(s, args...)
+        for arg in args
+            s = Base.replace(s, arg)
+        end
+        return s
+    end
 end
